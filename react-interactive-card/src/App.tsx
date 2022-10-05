d7bc8e2feb2749b893db2b9ac05b5d55ecc7a765
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-10-05 11:10:57 -0500

    Added Info comp

--- react-interactive-card/src/App.tsx
+++ react-interactive-card/src/App.tsx
@@ -1,10 +1,38 @@
 import Card from "./Card";
+import { useForm, FormProvider } from "react-hook-form";
+
+type FormInputs = {
+  theName: string;
+  cardNumber: string;
+  theDate: string;
+  verCode: string;
+};
 
 function App() {
+  const methods = useForm<FormInputs>({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    defaultValues: {},
+    resolver: undefined,
+    context: undefined,
+    criteriaMode: "firstError",
+    shouldFocusError: true,
+    shouldUnregister: false,
+    shouldUseNativeValidation: false,
+    delayError: undefined,
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <Card />
-    </div>
+    <FormProvider {...methods}>
+      <div className="h-screen">
+        <Card />
+        <form onChange={methods.handleSubmit(onSubmit)}></form>
+      </div>
+    </FormProvider>
   );
 }
 
